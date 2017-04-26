6b3ba513e65238f253e92a8c2f6f6976c2863041
Charlie Stopek <cstopek@hotmail.com>
2017-04-26 11:11:24 -0400

    Done.

--- index.js
+++ index.js
@@ -11,6 +11,18 @@ const robots = [
   { name: 'Ratchet', alliance: null }
 ];
 
+var sortedRobots = robots.map((autobot) => {
+  if(knownDecepticons.includes(autobot.name)){
+    return Object.assign({}, autobot, {
+      alliance: "decepticon"
+    })
+  } else {
+    return Object.assign({}, autobot, {
+      alliance: "autobot"
+    })
+  }
+})
+
 const zebraStripes = [
   { width: 9.12, color: null },
   { width: 5.71, color: null },
@@ -21,3 +33,15 @@ const zebraStripes = [
   { width: 0.59, color: null },
   { width: 7.31, color: null }
 ];
+
+var coloredZebraStripes = zebraStripes.map((stripe, index) => {
+  if(index % 2 === 0){
+    return Object.assign({}, stripe, {
+      color: "black"
+    })
+  }else {
+    return Object.assign({}, stripe, {
+      color: 'white'
+    })
+  }
+})
